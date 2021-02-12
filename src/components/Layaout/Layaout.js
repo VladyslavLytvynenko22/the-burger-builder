175d921f85927a1795597bc60828bd52d462fac0
Vladyslav Lytvynenko <vladyslav.lytvynenko22@gmail.com>
2021-02-12 23:53:24 +0200

    Adding a Toolbar

--- src/components/Layaout/Layaout.js
+++ src/components/Layaout/Layaout.js
@@ -2,10 +2,11 @@ import React from 'react';
 
 import Aux from './../../hoc/Auxiliary';
 import classes from './Layaout.module.css';
+import Toolbar from './../Navigation/Toolbar/Toolbar';
 
 const layaout = (props) => (
   <Aux>
-    <div>Toolbar, SideDrawer, Baxkdrop</div>
+    <Toolbar />
     <main className={classes.Content}>{props.children}</main>
   </Aux>
 );
