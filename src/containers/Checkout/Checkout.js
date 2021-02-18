7f30a4b4183cb18e6643c780615c5c6d47a15882
Vladyslav Lytvynenko <vladyslav.lytvynenko22@gmail.com>
2021-02-19 01:10:06 +0200

    Order Submission & Passing Data Between Pages

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -6,21 +6,22 @@ import { Route } from 'react-router-dom';
 
 export default class Checkout extends Component {
   state = {
-    ingredients: {
-      salad: 1,
-      meat: 1,
-      bacon: 1,
-      cheese: 1,
-    },
+    ingredients: null,
+    price: 0,
   };
 
-  componentDidMount() {
+  componentWillMount() {
     const query = new URLSearchParams(this.props.location.search);
     const ingredients = {};
+    let price = null;
     for (const param of query.entries()) {
-      ingredients[param[0]] = +param[1];
+      if (param[0] === 'price') {
+        price = param[1];
+      } else {
+        ingredients[param[0]] = +param[1];
+      }
     }
-    this.setState({ ingredients: ingredients });
+    this.setState({ ingredients: ingredients, price: price });
   }
 
   checkoutContinuedHandler = () => {
@@ -41,7 +42,13 @@ export default class Checkout extends Component {
         />
         <Route
           path={this.props.match.path + '/contact-data'}
-          component={ContactData}
+          render={() => (
+            <ContactData
+              ingredients={this.state.ingredients}
+              price={this.state.price}
+              {...this.props}
+            />
+          )}
         />
       </div>
     );
