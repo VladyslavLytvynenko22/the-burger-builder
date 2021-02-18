bde444bc63f116230f203455b531b681c8d2b78d
Vladyslav Lytvynenko <vladyslav.lytvynenko22@gmail.com>
2021-02-19 00:00:57 +0200

    Navigating Back & To Next Page

--- src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -10,10 +10,10 @@ const checkoutSummary = (props) => {
       <div className={classes.Checkout}>
         <Burger ingredients={props.ingredients} />
       </div>
-      <Button btnType='Danger' clicked>
+      <Button btnType='Danger' clicked={props.checkoutCancelled}>
         CANCEL
       </Button>
-      <Button btnType='Success' clicked>
+      <Button btnType='Success' clicked={props.checkoutContinued}>
         CONTINUE
       </Button>
     </div>
--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -11,10 +11,23 @@ export default class Checkout extends Component {
       cheese: 1,
     },
   };
+
+  checkoutContinuedHandler = () => {
+    this.props.history.replace('/checkout/contact-data');
+  };
+
+  checkoutCancelledHandler = () => {
+    this.props.history.goBack();
+  };
+
   render() {
     return (
       <div>
-        <CheckoutSummary ingredients={this.state.ingredients} />
+        <CheckoutSummary
+          ingredients={this.state.ingredients}
+          checkoutContinued={this.checkoutContinuedHandler}
+          checkoutCancelled={this.checkoutCancelledHandler}
+        />
       </div>
     );
   }
