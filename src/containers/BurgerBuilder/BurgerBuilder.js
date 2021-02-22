b9450e381b145649171f20fab3f6d4bee296c1ec
Vladyslav Lytvynenko <vladyslav.lytvynenko22@gmail.com>
2021-02-22 22:45:56 +0200

    Redux & UI State

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -14,7 +14,6 @@ import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 
 class BurgerBuilder extends Component {
   state = {
-    purchaseable: false,
     purchasing: false,
     loading: false,
     error: false,
@@ -42,9 +41,7 @@ class BurgerBuilder extends Component {
         return sum + el;
       }, 0);
 
-    this.setState({
-      purchaseable: sum > 0,
-    });
+    return sum > 0;
   }
 
   purchaseHandler = () => {
@@ -101,7 +98,7 @@ class BurgerBuilder extends Component {
             ingredientRemove={this.props.onIngredientRemoved}
             disabled={disabledInfo}
             price={this.props.totalPrice}
-            purchaseable={this.state.purchaseable}
+            purchaseable={this.updatePurchaseState(this.props.ingredients)}
             ordered={this.purchaseHandler}
           />
         </Aux>
