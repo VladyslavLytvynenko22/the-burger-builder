8bb0f5c6653c304dee63769cf9c3182be7bce6a6
Vladyslav Lytvynenko <vladyslav.lytvynenko22@gmail.com>
2021-02-23 21:01:59 +0200

    Adding an Auth Form

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 
+import Auth from './containers/Auth/Auth';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
 import Layaut from './hoc/Layaout/Layaout';
@@ -15,6 +16,7 @@ export default class App extends Component {
           <Switch>
             <Route path='/checkout' component={Checkout} />
             <Route path='/orders' component={Orders} />
+            <Route path='/auth' component={Auth} />
             <Route path='/' exact component={BurgerBuilder} />
           </Switch>
         </Layaut>
---
+++ src/containers/Auth/Auth.js
@@ -0,0 +1,115 @@
+import React, { Component } from 'react';
+
+import Button from './../../components/UI/Button/Button';
+import Input from './../../components/UI/Input/Input';
+import classes from './Auth.module.css';
+
+export default class Auth extends Component {
+  state = {
+    controls: {
+      email: this.getInputTypeObject('email', 'Mail Addres', '', {
+        required: true,
+        isEmail: true,
+      }),
+      password: this.getInputTypeObject('input', 'Password', '', {
+        required: true,
+        minLength: 6,
+      }),
+    },
+    formIsValid: false,
+  };
+
+  getInputTypeObject(type, placeholder, value, validations) {
+    return {
+      elementType: 'input',
+      elementConfig: {
+        type: type,
+        placeholder: placeholder,
+      },
+      value: value,
+      validation: {
+        ...validations,
+      },
+      valid: false,
+      touched: false,
+    };
+  }
+
+  checkValidity(value, rules) {
+    let isValid = true;
+    if (rules.required) {
+      isValid = value.trim() !== '';
+    }
+    if (rules.minLength && isValid) {
+      isValid = value.length >= rules.minLength;
+    }
+    if (rules.maxLength && isValid) {
+      isValid = value.length <= rules.maxLength;
+    }
+    if (rules.isEmail && isValid) {
+      const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      isValid = pattern.test(String(value).toLowerCase());
+    }
+    return isValid;
+  }
+
+  inputChangedHandlet = (event, controlName) => {
+    const updatedControls = {
+      ...this.state.controls,
+      [controlName]: {
+        ...this.state.controls[controlName],
+        value: event.target.value,
+        valid: this.checkValidity(
+          event.target.value,
+          this.state.controls[controlName].validation
+        ),
+        touched: true,
+      },
+    };
+
+    let formIsValid = true;
+    for (const inputIdentifier in updatedControls) {
+      formIsValid =
+        (updatedControls[inputIdentifier].valid ||
+          !updatedControls[inputIdentifier].validation) &&
+        formIsValid;
+    }
+
+    this.setState({
+      controls: updatedControls,
+      formIsValid: formIsValid,
+    });
+  };
+
+  render() {
+    const formElementsArray = [];
+    for (const key in this.state.controls) {
+      formElementsArray.push({
+        id: key,
+        config: this.state.controls[key],
+      });
+    }
+    const form = formElementsArray.map((formElement) => (
+      <Input
+        key={formElement.id}
+        elementType={formElement.config.elementType}
+        elementConfig={formElement.config.elementConfig}
+        value={formElement.config.value}
+        changed={(event) => this.inputChangedHandlet(event, formElement.id)}
+        invalid={!formElement.config.valid}
+        shouldValidate={formElement.config.validation}
+        touched={formElement.config.touched}
+      />
+    ));
+    return (
+      <div className={classes.Auth}>
+        <form onSubmit={this.orderHandler}>
+          {form}
+          <Button btnType='Success' disabled={!this.state.formIsValid}>
+            SUBBMIT
+          </Button>
+        </form>
+      </div>
+    );
+  }
+}
