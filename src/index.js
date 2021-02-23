84ea91ffee4e1d29847eb4af796072fcacb0f9ab
Vladyslav Lytvynenko <vladyslav.lytvynenko22@gmail.com>
2021-02-23 11:48:43 +0200

    Executing Asynchronous Code

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 import './index.css';
 
+import { applyMiddleware, compose, createStore } from 'redux';
+
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import burgerBuilderReducer from './store/reducers/burgerBuilder';
-import { createStore } from 'redux';
+import thunk from 'redux-thunk';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
   burgerBuilderReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  composeEnhancers(applyMiddleware(thunk))
 );
 
 const app = (
