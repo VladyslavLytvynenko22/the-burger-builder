856fe9c386acd53e7a7733001bd97a54c9bf9cdc
Vladyslav Lytvynenko <vladyslav.lytvynenko22@gmail.com>
2021-02-25 22:37:07 +0200

    Guarding Routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import * as actions from './store/actions/index';
 
 import React, { Component } from 'react';
-import { Route, Switch, withRouter } from 'react-router-dom';
+import { Redirect, Route, Switch, withRouter } from 'react-router-dom';
 
 import Auth from './containers/Auth/Auth';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
@@ -12,6 +12,12 @@ import Orders from './containers/Orders/Orders';
 import classes from './App.module.css';
 import { connect } from 'react-redux';
 
+const mapStateToProps = (state) => {
+  return {
+    isAuthenticated: state.auth.idToken !== null,
+  };
+};
+
 const mapDispatchToProps = (dispatch) => {
   return {
     onTryAuthSignup: () => dispatch(actions.authCheckSate()),
@@ -20,7 +26,7 @@ const mapDispatchToProps = (dispatch) => {
 
 export default withRouter(
   connect(
-    null,
+    mapStateToProps,
     mapDispatchToProps
   )(
     class App extends Component {
@@ -29,17 +35,30 @@ export default withRouter(
       }
 
       render() {
+        let routes = (
+          <Switch>
+            <Route path='/auth' component={Auth} />
+            <Route path='/' exact component={BurgerBuilder} />
+            <Redirect to='/' />
+          </Switch>
+        );
+
+        if (this.props.isAuthenticated) {
+          routes = (
+            <Switch>
+              <Route path='/checkout' component={Checkout} />
+              <Route path='/orders' component={Orders} />
+              <Route path='/auth' component={Auth} />
+              <Route path='/logout' component={Logout} />
+              <Route path='/' exact component={BurgerBuilder} />
+              <Redirect to='/' />
+            </Switch>
+          );
+        }
+
         return (
           <div className={classes.App}>
-            <Layaut>
-              <Switch>
-                <Route path='/checkout' component={Checkout} />
-                <Route path='/orders' component={Orders} />
-                <Route path='/auth' component={Auth} />
-                <Route path='/logout' component={Logout} />
-                <Route path='/' exact component={BurgerBuilder} />
-              </Switch>
-            </Layaut>
+            <Layaut>{routes}</Layaut>
           </div>
         );
       }
