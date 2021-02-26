c182a18c77f921452ce7e6ce88db4d5c8b0f2856
Vladyslav Lytvynenko <vladyslav.lytvynenko22@gmail.com>
2021-02-26 12:05:50 +0200

    Jest and Enzyme Documentations

--- src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -8,17 +8,24 @@ import React from 'react';
 configure({ adapter: new Adapter() });
 
 describe('<NavigationItem />', () => {
-  let wraper;
+  let wrapper;
   beforeEach(() => {
-    wraper = shallow(<NavigationItems />);
+    wrapper = shallow(<NavigationItems />);
   });
 
   it('should render two <NavigationItem /> elements if not authenticated', () => {
-    expect(wraper.find(NavigationItem)).toHaveLength(2);
+    expect(wrapper.find(NavigationItem)).toHaveLength(2);
   });
 
-  it('should render two <NavigationItem /> elements if authenticated', () => {
-    wraper.setProps({ isAuthenticated: true });
-    expect(wraper.find(NavigationItem)).toHaveLength(3);
+  it('should render three <NavigationItem /> elements if authenticated', () => {
+    wrapper.setProps({ isAuthenticated: true });
+    expect(wrapper.find(NavigationItem)).toHaveLength(3);
+  });
+
+  it('should render <NavigationItem link="/logout"/> elements if authenticated', () => {
+    wrapper.setProps({ isAuthenticated: true });
+    expect(
+      wrapper.contains(<NavigationItem link='/logout'>Logout</NavigationItem>)
+    ).toEqual(true);
   });
 });
